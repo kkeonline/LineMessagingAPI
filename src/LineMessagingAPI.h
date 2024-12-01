15a01b6af9e7d77a181ced964dffe5518ff0692c
KKE <30925725+kkeonline@users.noreply.github.com>
2024-12-02 04:34:02 +0700

    Create LineMessagingAPI.h

---
+++ src/LineMessagingAPI.h
@@ -0,0 +1,34 @@
+/*
+Line Messaging API for ESP8266
+*/
+
+#ifndef LineMessagingAPI_H
+#define LineMessagingAPI_H
+
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClientSecure.h>
+  #define HARDWARE "esp8266"
+
+class LineMessagingAPI{
+  private:
+    String _TOKEN = "";
+    String _USERID = "";
+//  String _API_URL = "https://api.line.me/v2/bot/message/push";
+    String _API_HOST = "api.line.me";
+    String _API_PATH = "/v2/bot/message/push";
+
+  public:
+    int status_code = 0;
+
+    LineMessagingAPI();
+
+    void begin(String Channel_access_token = "");
+    void begin(String Channel_access_token = "", String ReceiverUserID = "");
+    void setToken(String Channel_access_token);
+    void setUserID(String ReceiverUserID);
+    bool notify(String message);
+};
+
+extern LineMessagingAPI LINE;
+#endif
